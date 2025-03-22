50185823282a666485248ce382d2c34bdf248fc2
MartvStraten <131674849+MartvStraten@users.noreply.github.com>
2025-03-22 18:53:46 +0100

    update GUI

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot as plt
 import SimpleITK as sitk
 import numpy as np
 import cv2
 import os
+import io
 
 import tkinter as tk
 import tkinter.messagebox
@@ -87,7 +89,7 @@ class App(ctk.CTk):
         )
         self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
 
-        # MAIN ENTRY WIDGET AND BUTTON -------------------------------------------------------------------------------------------
+        # BOTTOM ENTRY WIDGET AND BUTTON -----------------------------------------------------------------------------------------
         self.entry = ctk.CTkEntry(self, 
             placeholder_text="Search"
         )
@@ -100,78 +102,95 @@ class App(ctk.CTk):
         )
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-        # IMAGES -----------------------------------------------------------------------------------------------------------------
-        self.image_frame = ctk.CTkFrame(self, corner_radius=0)
-        self.image_frame.grid(row=0, column=1, padx=(20,0), pady=(20, 0), sticky="nsew") 
-        self.image_frame.grid_columnconfigure((0, 1, 2), weight=1)
-        self.image_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        # DICOM VIEWER ------------------------------------------------------------------------------------------------------------
+        self.image_tabview = ctk.CTkTabview(self, corner_radius=0)
+        self.image_tabview.grid(row=0, column=1, padx=(20,0), pady=(20, 0), sticky="nsew") 
+
+        self.image_tabview.add("DICOM viewer")
+        self.dicom_viewer_frame = self.image_tabview.tab("DICOM viewer")
+        self.dicom_viewer_frame.grid_columnconfigure((0, 1, 2), weight=1)
+        self.dicom_viewer_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
         # Sagittal DICOM image label
-        self.sagittal_dicom_image_label = tk.Label(self.image_frame,
+        self.sagittal_dicom_image_label = tk.Label(self.dicom_viewer_frame,
             bg="gray10"
         )
         self.sagittal_dicom_image_label.grid(row=0, column=0, padx=10, pady=10)
-
         # Coronal DICOM image label
-        self.coronal_dicom_image_label = tk.Label(self.image_frame, 
+        self.coronal_dicom_image_label = tk.Label(self.dicom_viewer_frame, 
             bg="gray10"
         )
         self.coronal_dicom_image_label.grid(row=0, column=1, padx=10, pady=10)
-
         # Axial DICOM image label
-        self.axial_dicom_image_label = tk.Label(self.image_frame, 
+        self.axial_dicom_image_label = tk.Label(self.dicom_viewer_frame, 
             bg="gray10"
         )
         self.axial_dicom_image_label.grid(row=0, column=2, padx=10, pady=10)
 
-        # SLIDER FOR DICOM SLICE SELECTION ---------------------------------------------------------------------------------------
         # Start slice indices in the middle
         self.sagittal_slice_idx = 50
         self.coronal_slice_idx = 336
         self.axial_slice_idx = 336
 
         # Sagittal slider widget
-        self.sagittal_slider = ctk.CTkSlider(self.image_frame, 
+        self.sagittal_slider = ctk.CTkSlider(self.dicom_viewer_frame, 
             from_=0, 
             to=100, 
             command=self.update_slice_sagittal
         )
-        self.sagittal_slider_text = ctk.CTkLabel(self.image_frame, 
+        self.sagittal_slider_text = ctk.CTkLabel(self.dicom_viewer_frame, 
             text=f"Sagittal slice {self.sagittal_slice_idx}"
         )
-
         # Coronal slider widget
-        self.coronal_slider = ctk.CTkSlider(self.image_frame, 
+        self.coronal_slider = ctk.CTkSlider(self.dicom_viewer_frame, 
             from_=0, 
             to=671, 
             command=self.update_slice_coronal
         )
-        self.coronal_slider_text = ctk.CTkLabel(self.image_frame, 
+        self.coronal_slider_text = ctk.CTkLabel(self.dicom_viewer_frame, 
             text=f"Coronal slice {self.coronal_slice_idx}"
         )
-
         # Axial slider widget
-        self.axial_slider = ctk.CTkSlider(self.image_frame, 
+        self.axial_slider = ctk.CTkSlider(self.dicom_viewer_frame, 
             from_=0, 
             to=671, 
             command=self.update_slice_axial
         )
-        self.axial_slider_text = ctk.CTkLabel(self.image_frame, 
+        self.axial_slider_text = ctk.CTkLabel(self.dicom_viewer_frame, 
             text=f"Axial slice {self.axial_slice_idx}"
         )
 
-        # Create tabview
-        self.tabview = ctk.CTkTabview(self, width=300)
-        self.tabview.grid(row=0, column=3, padx=(20, 10), pady=(20, 0), sticky="nsew")
-        self.tabview.add("Tab 1")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("Tab 1").grid_columnconfigure(0, weight=1) 
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        # RIGHT SIDE BAR ---------------------------------------------------------------------------------------------------------
+        self.right_tabview = ctk.CTkTabview(self, width=300)
+        self.right_tabview.grid(row=0, column=3, padx=(20, 10), pady=(20, 0), sticky="nsew")
 
+        self.right_tabview.add("Vertebra")
+        self.vertebra_frame = self.right_tabview.tab("Vertebra")
+        self.vertebra_frame.grid_columnconfigure(0, weight=1) 
+        self.vertebra_frame.grid_rowconfigure((0, 1), weight=0) 
+
+        # Text label
+        self.target_vertebra_text = ctk.CTkLabel(self.vertebra_frame,
+            text="Select target vertebra"
+        )
+        self.target_vertebra_text.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
+
+        # Option menu for target vertebra
+        self.vertebra_mask = None
+        self.vertebra = ["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12", "L1", "L2", "L3", "L4", "L5"]
+        self.optionmenu = ctk.CTkOptionMenu(self.vertebra_frame,
+            values=self.vertebra,
+            command=self.vertebra_select
+        )
+        self.optionmenu.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="ew")
+
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        """Changes GUI appearance."""
+        ctk.set_appearance_mode(new_appearance_mode)
 
     def select_patient_ct(self):
-        """Open directory dialog and store the selected folder path"""
+        """Open directory dialog and store the selected folder path."""
         folder_path = filedialog.askdirectory(title="Select DICOM folder")
         if folder_path:
             self.dicom_path = folder_path
@@ -180,7 +199,7 @@ class App(ctk.CTk):
             self.sidebar_button_3.configure(state="normal")
 
     def select_segmentation(self):
-        """Open file dialog and store the selected file path"""
+        """Open file dialog and store the selected file path."""
         file_path = filedialog.askopenfilename(title="Select segmentation file")
         if file_path:
             patient_id = "_".join(os.path.basename(file_path).split("_")[:-1])
@@ -195,7 +214,7 @@ class App(ctk.CTk):
                     self.sidebar_button_3.configure(state="normal")
 
     def load_data(self):
-        """Load patient data and update image frame"""
+        """Load patient data and update image frame."""
         # Load dicom data and apply windowing
         self.dicom_image = load_3d_dicom(self.dicom_path)
         self.dicom_image = apply_window(self.dicom_image, window_level=450, window_width=1500)
@@ -223,7 +242,7 @@ class App(ctk.CTk):
         self.axial_slider_text.grid(row=3, column=2)
 
     def update_slice_sagittal(self, value):
-        """Update the displayed slice based on the slider value"""
+        """Update the displayed slice based on the slider value."""
         self.sagittal_slice_idx = int(float(value))
         self.upload_image_to_label(
             self.sagittal_dicom_image_label, 
@@ -232,7 +251,7 @@ class App(ctk.CTk):
         self.sagittal_slider_text.configure(text=f"Sagittal slice {self.sagittal_slice_idx}")
 
     def update_slice_axial(self, value):
-        """Update the displayed slice based on the slider value"""
+        """Update the displayed slice based on the slider value."""
         self.axial_slice_idx = int(float(value))
         self.upload_image_to_label(
             self.axial_dicom_image_label, 
@@ -241,7 +260,7 @@ class App(ctk.CTk):
         self.axial_slider_text.configure(text=f"Axial slice {self.axial_slice_idx}")
 
     def update_slice_coronal(self, value):
-        """Update the displayed slice based on the slider value"""
+        """Update the displayed slice based on the slider value."""
         self.coronal_slice_idx = int(float(value))
         self.upload_image_to_label(
             self.coronal_dicom_image_label, 
@@ -249,14 +268,36 @@ class App(ctk.CTk):
         )
         self.coronal_slider_text.configure(text=f"Coronal slice {self.coronal_slice_idx}")
 
-    def upload_image_to_label(self, label, image):       
+    def upload_image_to_label(self, label, image): 
+        """Uploading images to label widgets."""      
         # Prepare image object
-        image_tk = Image.fromarray(image) 
-        image_tk = ImageTk.PhotoImage(image_tk)
-
-        # Place image in widget
+        if isinstance(image, np.ndarray):
+            # Convert NumPy array to PIL Image
+            image_PIL = Image.fromarray(image)
+        elif isinstance(image, plt.Figure):
+            # Convert matplotlib figure to PIL Image
+            buf = io.BytesIO()
+            image.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+            buf.seek(0)
+            image_PIL = Image.open(buf)
+        
+        # Turn image into photo and place in widget
+        image_tk = ImageTk.PhotoImage(image_PIL)
         label.config(image=image_tk)
         label.image = image_tk
 
-    def change_appearance_mode_event(self, new_appearance_mode):
-        ctk.set_appearance_mode(new_appearance_mode)
+    def vertebra_select(self, choice):
+        """Selection of target vertebra and update DICOM viewer."""
+        # Translate vertebra to label
+        vertebra_to_label = {"T1":1, "T2":2, "T3":3, "T4":4, "T5":5, "T6":6, "T7":7, "T8":8, "T9":9, "T10":10, "T11":11, "T12":12, "L1":13, "L2":14, "L3":15, "L4":16, "L5":17}
+        self.target_vertebra = vertebra_to_label[choice]
+
+        # Update DICOM viewer
+        self.vertebra_mask = (self.segment_data == self.target_vertebra)
+
+    def generate_contour_plot(self, segmentation_slice):
+        """Generate a contour plot of the segmentation slice using matplotlib."""
+        fig = plt.figure()
+        plt.contour(segmentation_slice)
+        plt.axis('off')
+        return fig
